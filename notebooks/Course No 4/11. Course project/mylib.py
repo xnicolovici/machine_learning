daa28d70473c9e4e30ae589ebd6e476a187d7d2c
Xavier Nicolovici <xavier@nicolovici.com>
2021-05-16 00:00:41 +0200

    Project #4 - K-NN classifiers done

--- notebooks/Course No 4/11. Course project/mylib.py
+++ notebooks/Course No 4/11. Course project/mylib.py
@@ -5,7 +5,7 @@ def loadNpz(filename=os.path.join('data','data.npz')):
     """
     This function returns the content of the NPZ file passed as parameter.
     The NPZ file passed must been build according to the first Notebook process of this project
-    The value returned as a structure described above
+    The value returned a structure that is described in the Notebook 02 - Data exploration.ipynb
     """
     
     # initialize returned variable
@@ -31,6 +31,19 @@ def loadNpz(filename=os.path.join('data','data.npz')):
                 print('     shape: {} - dtype: {}'.format(data_dict[name][data_type].shape, data_dict[name][data_type].dtype))
 
             print("\n")
+
+        name='trainX'
+        print("building '{}' set".format(name))
+        data_dict[name]=dict()
+            
+        for data_type in ['data', 'features', 'filenames', 'labels']:
+            print("  building ", data_type)
+            data_dict[name][data_type]=np.concatenate((data_dict['train'][data_type], data_dict['valid'][data_type]), axis=0)
+            print('     shape: {} - dtype: {}'.format(data_dict[name][data_type].shape, data_dict[name][data_type].dtype))
+
+        print("\n")
+
+
             
         # Return the structure prepared by this function
         return data_dict
@@ -38,8 +51,9 @@ def loadNpz(filename=os.path.join('data','data.npz')):
 
 def loadXy(data=None, concatenate=[]):
     """
-    This function returns the data, X_train, y_train, X_valid, y_valid, X_test and y_test vectors
+    This function returns the data, an X and a y dict containing 'train', 'valid', 'test' and 'trainX' vectors
     from the data passed as parameter.
+    The 'trainX' is the merge of the 'train' and 'valid' dataset.
     If the data parameter is set to None, this function uses the loadNpz() function with default parameter
     to get the data.
     Note that the data passed as parameter must comply with the structure passed in Notebook number 1
@@ -54,7 +68,12 @@ def loadXy(data=None, concatenate=[]):
     for name in data['DATASET_NAME']:
         X[name]=data[name]['features']
         y[name]=data[name]['labels']
-        print("X {} shape:".format(name),X[name].shape)
-        print("y {} shape:".format(name),X[name].shape)
+        print("X['{}'] shape:".format(name),X[name].shape)
+        print("y['{}'] shape:".format(name),X[name].shape)
 
+    X['trainX']=np.concatenate((X['train'], X['valid']), axis=0)
+    y['trainX']=np.concatenate((y['train'], y['valid']), axis=0)
+    print("X['trainX'] shape:", X['trainX'].shape)
+    print("y['trainX'] shape:", X['trainX'].shape)
+    
     return (data, X, y)
